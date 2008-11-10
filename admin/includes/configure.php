0e832c93d04df9085a74029233e1b9276c3db255
Frederick Alger <fred@glug.localdomain>
2008-11-10 14:20:09 -0800

    Got customer creation working; creates a correct address book entry and inserts the password hash in the right spot.

--- admin/includes/configure.php
+++ admin/includes/configure.php
@@ -1,27 +1,13 @@
 <?php
-/*
-  $Id: configure.php 1739 2007-12-20 00:52:16Z hpdl $
-
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2002 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-// define our webserver variables
-// FS = Filesystem (physical)
-// WS = Webserver (virtual)
-  define('HTTP_SERVER', ''); // eg, http://localhost or - https://localhost should not be NULL for productive servers
-  define('HTTP_CATALOG_SERVER', '');
-  define('HTTPS_CATALOG_SERVER', '');
-  define('ENABLE_SSL_CATALOG', 'false'); // secure webserver for catalog module
-  define('DIR_FS_DOCUMENT_ROOT', $DOCUMENT_ROOT); // where your pages are located on the server. if $DOCUMENT_ROOT doesnt suit you, replace with your local path. (eg, /usr/local/apache/htdocs)
+  define('HTTP_SERVER', 'http://fauxsc.dev');
+  define('HTTP_CATALOG_SERVER', 'http://fauxsc.dev');
+  define('HTTPS_CATALOG_SERVER', 'http://fauxsc.dev');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', '/home/projects/fauxsc/catalog/');
   define('DIR_WS_ADMIN', '/admin/');
-  define('DIR_FS_ADMIN', DIR_FS_DOCUMENT_ROOT . DIR_WS_ADMIN);
-  define('DIR_WS_CATALOG', '/catalog/');
-  define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG);
+  define('DIR_FS_ADMIN', '/home/projects/fauxsc/catalog/admin/');
+  define('DIR_WS_CATALOG', '/');
+  define('DIR_FS_CATALOG', '/home/projects/fauxsc/catalog/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -37,11 +23,10 @@
   define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
   define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
 
-// define our database connection
-  define('DB_SERVER', '');
-  define('DB_SERVER_USERNAME', 'mysql');
-  define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'osCommerce');
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'fauxsc');
+  define('DB_SERVER_PASSWORD', 'bl0op3r.');
+  define('DB_DATABASE', 'fauxsc_development');
   define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', '');
+  define('STORE_SESSIONS', 'mysql');
 ?>
